58734ff53b70331135e63213041178993cbcb368
nmateyko <nick.mateyko@gmail.com>
2024-11-18 11:43:50 -0800

    Update version, add requirements and extra info

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
 
 setup(
-    name = 'labutils',
-    version = '1.0.0',
-    packages = ['sequence_analysis'],
-    entry_points = {
-            'console_scripts': [
-            'get_plasmid_inserts = sequence_analysis.get_plasmid_inserts:main',
+    name="labutils",
+    version="1.1.0",
+    packages=['sequence_analysis'],
+    install_requires=['edlib'],
+    entry_points={
+        "console_scripts": [
+            "get_plasmid_inserts=sequence_analysis.get_plasmid_inserts:main",
         ],
     },
+    test_suite='tests',
+    author="Nick Mateyko",
+    author_email="nick.mateyko@gmail.com",
+    description="Various scripts, mostly for sequence analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nmateyko/labutils",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
